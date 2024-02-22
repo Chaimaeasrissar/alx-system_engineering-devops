d7423c6016c2428da1e2fdc8a485f93be88fbb84
chaimaeasrissar <chaimaeasrissar@gmail.com>
2024-02-22 14:13:00 -0800

    a

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,8 @@
-# make the limit higer
-exec { 'file limit':
-  onlyif   => 'test -e /etc/default/nginx',
-  command  => 'sed -i 5s/[0-9]+/1048576/ /etc/default/nginx; service nginx restart',
-  provider => shell,
+#0-the_sky_is_the_limit_not.pp set open file limit higher
+exec { 'set limit to 2000':
+  path    => '/bin',
+  command => sed -i s/15/2000/ /etc/default/nginx
+}
+exec { 'reboot nginx':
+  command => '/usr/sbin/service nginx restart'
 }
