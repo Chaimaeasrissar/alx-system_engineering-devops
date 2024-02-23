3c855c4928caaa040004ae669d384487d068ad51
chaimaeasrissar <chaimaeasrissar@gmail.com>
2024-02-22 17:35:55 -0800

    a

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,12 +1,9 @@
-# loggin with holberton user
-
-
-exec { 'Modify hard limit':
-    command  => 'sed -i s/holberton hard nofile 5/holberton hard nofile unlimited/g /etc/security/limits.conf',
-    provider => 'shell'
+# obligatory puppet comment. 
+exec { 'hard limit':
+  command => "sed -i 's/5/4000/' /etc/security/limits.conf",
+  path    => '/bin'
 }
-
-exec { 'Modify soft limit':
-    command  => 'sed -i s/holberton soft nofile 4/holberton soft nofile 3000/g /etc/security/limits.conf',
-    provider => 'shell'
+exec { 'soft limit':
+  command => "sed -i 's/4/2000/' /etc/security/limits.conf",
+  path    => '/bin'
 }
